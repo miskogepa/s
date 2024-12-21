45133a1f7c83de0f0a461ba63732728341d80f2c
miskogepa <milovan.jankovic50m@gmail.com>
2024-12-21 15:40:14 +0100

    zavrsena prva strana

---
+++ s.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const inputs = document.querySelectorAll('.password input');
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', () => {
+            if (input.value.length === input.maxLength) {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                }
+            }
+        });
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && input.value === '') {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    });
+});
