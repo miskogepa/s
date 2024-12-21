85b927703e79e249844b6ffdaec2f727e0a96d2f
miskogepa <milovan.jankovic50m@gmail.com>
2024-12-21 15:49:59 +0100

    dodat kartica i password

--- s.js
+++ s.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const inputs = document.querySelectorAll('.password input');
+    const correctPassword = '300824';
     inputs.forEach((input, index) => {
         input.addEventListener('input', () => {
             if (input.value.length === input.maxLength) {
@@ -7,6 +8,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     inputs[index + 1].focus();
                 }
             }
+            checkPassword();
         });
         input.addEventListener('keydown', (e) => {
             if (e.key === 'Backspace' && input.value === '') {
@@ -16,4 +18,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         });
     });
+
+    function checkPassword() {
+        const enteredPassword = Array.from(inputs).map(input => input.value).join('');
+        if (enteredPassword === correctPassword) {
+            document.getElementById('text').style.display = 'none';
+            document.getElementById('password').style.display = 'none';
+            document.querySelector('.card').style.display = 'block';
+        }
+    }
 });
